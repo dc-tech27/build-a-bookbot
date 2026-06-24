2bc4c0cd55cf649a6903afdf83d72359e2b5eccc
david <dchordigian.tech@gmail.com>
2026-06-24 15:48:38 -0700

    added print report function to main.py to output a clean character report

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ def main() -> None:
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print(f"Found {num_words} total words")
-    print(chars_sorted_list)
+    report = print_report(book_path, num_words, chars_sorted_list)
 
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_count in chars_sorted_list:
+        if char_count[0].isalpha():
+            print(f"{char_count[0]}: {char_count[1]}")
+    print("============= END ===============")
 
 def get_book_text(path: str) -> str:
     with open(path) as f:
