dfd17bc104355b1bedb554accc17324c3794e773
david <dchordigian.tech@gmail.com>
2026-06-24 15:33:18 -0700

    added functions to stats.py to sort character dictionary into a tuple list and imported function into main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 def main() -> None:
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
     print(f"Found {num_words} total words")
-    print(chars_dict)
+    print(chars_sorted_list)
 
 
 def get_book_text(path: str) -> str:
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def get_chars_dict(text: str) -> dict[str, int]:
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(char_count: tuple[str, int]) -> int:
+    return char_count[1]
+
+def chars_dict_to_sorted_list(num_chars_dict: dict[str, int]) -> list[tuple[str, int]]:
+    chars_list: list[tuple[str, int]] = []
+    for char in num_chars_dict:
+        count = num_chars_dict[char]
+        chars_list.append((char, count))
+    return sorted(chars_list, reverse=True, key=sort_on)
